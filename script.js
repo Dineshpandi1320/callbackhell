59c9c00841827cc18d8167c1fe960751b0dc8d95
Dineshpandi1320 <dineshd1307@gmail.com>
2023-01-12 16:22:17 +0530

    change

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-// var timeleft = 10;
-// var downloadTimer = setInterval(function(){
-//   if(timeleft <= 0){
-//     clearInterval(downloadTimer);
-//     document.getElementById("countdown").innerHTML = "Finished";
-//   } else {
-//     document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
-//   }
-//   timeleft -= 1;
-// }, 1000);
-
-
 function lastCall(callback){
     document.getElementById("countdown").innerHTML = "10";
     setTimeout(callback,1000)
